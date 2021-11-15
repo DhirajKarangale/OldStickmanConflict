022d4a9fb336dab2e3a8cca11c86f5168c206e06
Dhiraj Karangale <dakarangale02@gmail.com>
2021-11-16 00:02:04 +0530

    Some Rocks added behind Ground, Both Camera Changes

--- Stickman Conflict/Assets/Scripts/Player/PlayerMovement.cs
+++ Stickman Conflict/Assets/Scripts/Player/PlayerMovement.cs
@@ -26,24 +26,23 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             rigidBody.velocity = new Vector2(Mathf.Clamp(rigidBody.velocity.x, -15, 12), Mathf.Clamp(rigidBody.velocity.y, -10, 12));
-            // Vector2 moveVelocity = rigidBody.velocity;
 
             // Move
             if (moveJoystick.Horizontal() > 0.4F) // Move Front
             {
+                animator.speed = 1.3f;
                 animator.Play("Walk");
                 DustEffect();
                 rigidBody.AddForce(Vector2.right * moveSpeed * moveJoystick.Horizontal());
-                // moveVelocity = new Vector2(moveSpeed * moveJoystick.Horizontal(), rigidBody.velocity.y);
                 weaponRotation = new Vector3(0, 0, -90);
                 head.localScale = new Vector3(0.67f, 0.7f, 1);
             }
             else if (moveJoystick.Horizontal() < -0.4F) // Move Back
             {
+                animator.speed = 1.3f;
                 animator.Play("Walk Back");
                 DustEffect();
                 rigidBody.AddForce(Vector2.right * moveSpeed * moveJoystick.Horizontal());
-                // moveVelocity = new Vector2(moveSpeed * moveJoystick.Horizontal(), rigidBody.velocity.y);
                 weaponRotation = new Vector3(0, 0, 90);
                 head.localScale = new Vector3(-0.67f, 0.7f, 1);
             }
@@ -51,17 +50,15 @@ public class PlayerMovement : MonoBehaviour
 
             // Jump & Crounch
             if ((moveJoystick.Vertical() > 0.6f) && (Physics2D.IsTouchingLayers(legCollider, 64))) // Jump 
-            {                                                                             // 64 is Layer Mask Of Ground
+            {                                                                             // 64 is Layer Mask Of Ground     
+                animator.speed = 0.2f;                                                           
                 DustEffect();
-                rigidBody.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);
-                //  moveVelocity = new Vector2(rigidBody.velocity.x, moveJoystick.Vertical() * jumpForce);
+                rigidBody.AddForce(jumpForce * Vector2.up * moveJoystick.Vertical(), ForceMode2D.Impulse);
             }
             else if (moveJoystick.Vertical() < -0.95f) // Crounch
             {
                 rigidBody.AddForce(Vector2.down * downForce * Time.deltaTime);
             }
-
-            //  rigidBody.velocity = moveVelocity;
         }
     }
 
